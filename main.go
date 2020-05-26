5bd7d7f5b87947e50fed7beb601c8e44d9aa6d4f
flavioespinoza <flavio.espinoza@gmail.com>
2020-05-26 08:53:55 -0600

    maingo

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-    "fmt"
-    "github.com/gorilla/mux"
     "log"
-    "net/http"
     "os"
     "text/template"
 )
@@ -16,26 +13,8 @@ func init() {
 }
 
 func main() {
-    r := mux.NewRouter()
-
-    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", 667)
-        if err != nil {
-            log.Fatalln(err)
-        }
-    })
-
-    r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "<h1>Hello from Docker!\n</h1>")
-    })
-
-    r.HandleFunc("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
-        vars := mux.Vars(r)
-        title := vars["name"]
-
-        fmt.Fprintf(w, "<h1>Hello, %s!\n</h1>", title)
-    })
-
-    http.ListenAndServe(":80", r)
+    err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", 42)
+    if err != nil {
+        log.Fatalln(err)
+    }
 }
-
--- scripts/start-go.sh
+++ scripts/start-go.sh
@@ -1,3 +1,4 @@
+
 echo -e "start go-app"
 
 docker-compose -f go-app-compose.yaml up -d
