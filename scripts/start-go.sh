72fc03dd271a504e6976bdceafed8f9537ce1e46
flavioespinoza <flavio.espinoza@gmail.com>
2020-05-26 08:14:42 -0600

    stuff

--- scripts/start-go.sh
+++ scripts/start-go.sh
@@ -1,3 +1,3 @@
 echo -e "start go-app"
 
-docker-compose -f go-app-compose.yaml up -d --remove-orphans
+docker-compose -f go-app-compose.yaml up -d
--- scripts/start-nginx.sh
+++ scripts/start-nginx.sh
@@ -1,1 +1,1 @@
-docker-compose -f nginx-proxy-compose.yaml up -d --remove-orphans
+docker-compose -f nginx-proxy-compose.yaml up -d
